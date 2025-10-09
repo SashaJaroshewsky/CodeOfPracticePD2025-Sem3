c36c08480a5b5029cee15b8b394975b49242c2e6
DESKTOP-BFGS2MG\jaros <jaroschew@gmail.com>
2025-10-09 13:26:16 +0300

    Add PZ3

---
+++ PD23/PZ3/Employees/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ3.Employees
+{
+    internal class Employee
+    {
+        protected decimal Salary { get; private set; }
+        public string Name { get; set; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void PaySalary(decimal money)
+        {
+            Salary += money;
+            // Example salary payment logic
+        }
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Працівник");
+        }
+
+        public virtual decimal GetSalary() { return Salary; }
+    }
+}
---
+++ PD23/PZ3/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ3
+{
+    internal class Notification
+    {
+
+        public void Notify(string message)
+        {
+            Console.WriteLine($"Notification: {message}");
+        }
+    }
+}
