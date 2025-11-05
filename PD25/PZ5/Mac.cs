58b315bc090a596b4fd3f5d595b7cd1720b02719
DESKTOP-BFGS2MG\jaros <jaroschew@gmail.com>
2025-11-05 16:32:19 +0200

    Add PZ5

---
+++ PD25/PZ5/Mac.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ5
+{
+    internal class Mac: Laptop
+    {
+        public Mac(string name) : base(name)
+        {
+        }
+    }
+  
+}
---
+++ PD25/PZ5/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace PZ5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Laptop laptop = new Laptop("MyLaptop");
+            Laptop laptop2 = new Laptop("MyLaptop2");
+            Phone phone = new Phone("MyPhone");
+            PC pc = new PC("MyPC");
+
+            List<Device> devices = new List<Device> { laptop, laptop2, phone, pc };
+
+            foreach (var device in devices)
+            {
+                //IChargeable chargeableDevice = (IChargeable)device;
+                if (device is IChargeable chargeableDevice)
+                {
+                    //IChargeable chargeableDevice = (IChargeable)device;
+                    chargeableDevice.Charge();
+                }
+                else
+                {
+                    Console.WriteLine($"{device.Name} не підтримує заряджання.");
+                }
+            }
+
+            Console.WriteLine("+++++++++++++++++++++");
+
+            foreach (var device in devices)
+            {
+                //IChargeable chargeableDevice = (IChargeable)device;
+                IChargeable chargeableDevice = device as IChargeable;
+                if (chargeableDevice != null)
+                {
+                    //IChargeable chargeableDevice = (IChargeable)device;
+                    chargeableDevice.Charge();
+                }
+                else
+                {
+                    Console.WriteLine($"{device.Name} не підтримує заряджання.");
+                }
+            }
+
+            foreach (var device in devices)
+            {
+                if (device is Laptop laptop1)
+                {
+                    Console.WriteLine("Це ноутбук");
+                }
+                else
+                {
+                    Console.WriteLine("Це не ноутбук");
+                }
+            }
+
+            Device device1 = new Laptop("MyLaptop3");
+
+            Laptop laptop4 = (Laptop)device1;
+
+
+
+        }
+    }
+}
